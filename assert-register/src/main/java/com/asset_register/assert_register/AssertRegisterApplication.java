783cae4892e73b41da52c7c11ca1800ce2f31822
Konanani Netsshifhefhe <konanani@oneconnect.co.za>
2024-12-09 18:51:51 +0200

    Add files via upload

---
+++ assert-register/src/main/java/com/asset_register/assert_register/AssertRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.asset_register.assert_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssertRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssertRegisterApplication.class, args);
+	}
+
+}
---
+++ assert-register/src/test/java/com/asset_register/assert_register/AssertRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asset_register.assert_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssertRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
